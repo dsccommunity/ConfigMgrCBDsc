28aebbb86c800b0c1e0a5a589249a8fe02953b7f
Nick Ellis <nicholas.ellis280@gmail.com>
2021-09-17 09:37:30 -0400

    Updating Reverse DSC Pester

--- tests/Unit/ConfigMgrCBDsc.ReverseDsc.tests.ps1
+++ tests/Unit/ConfigMgrCBDsc.ReverseDsc.tests.ps1
@@ -4012,7 +4012,7 @@ InModuleScope $script:subModuleName {
                 $result = Set-ConfigMgrCBDscReverse @testAll
                 $result | Should -BeOfType System.String
                 Assert-MockCalled Get-CMAccount -Exactly -Times 1 -Scope It
-                Assert-MockCalled Invoke-DscResource -Exactly -Times 27 -Scope It
+                Assert-MockCalled Invoke-DscResource -Exactly -Times 28 -Scope It
                 Assert-MockCalled Get-CMAdministrativeUser -Exactly -Times 1 -Scope It
                 Assert-MockCalled Get-CMAssetIntelligenceSynchronizationPoint -Exactly -Times 1 -Scope It
                 Assert-MockCalled Get-CMCollection -Exactly -Times 2 -Scope It
