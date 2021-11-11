fe73cf87d111fcbc31f4a21e414e4730f4101ebb
Nick Ellis <nicholas.ellis280@gmail.com>
2021-11-11 10:22:04 -0500

    Updating changelog and readme

--- source/DSCResources/xSccmPreReqs/xSccmPreReqs.schema.psm1
+++ source/DSCResources/xSccmPreReqs/xSccmPreReqs.schema.psm1
@@ -165,7 +165,6 @@ Configuration xSCCMPreReqs
     )
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
-    Import-DscResource -ModuleName NetworkingDsc -ModuleVersion 8.2.0
 
     $features = @()
     foreach ($role in $SccmRole)
